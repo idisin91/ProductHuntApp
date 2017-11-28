6dda2a7a8f614d97feee06bb8d20e285bccecdeb
idisin91 <idisin91@gmail.com>
2017-11-28 16:40:07 +0300

    added loading topics from web

---
+++ ProductHuntApp/ProductHuntApp/Controllers/EmptyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmptyTableViewCell.swift
+//  ProductHuntApp
+//
+//  Created by Никита Скрынников on 28.11.17.
+//  Copyright © 2017 ForMe. All rights reserved.
+//
+
+import UIKit
+
+class EmptyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ProductHuntApp/ProductHuntApp/Models/Topic.swift
@@ -0,0 +1,9 @@
+//
+//  Topic.swift
+//  ProductHuntApp
+//
+//  Created by Никита Скрынников on 28.11.17.
+//  Copyright © 2017 ForMe. All rights reserved.
+//
+
+import Foundation
